8edee014763d55304952356365f9d2dedcc8ed77
DevUser01Mechten <dev@mechten.eu>
2014-06-03 13:48:49 +0100

    Added bee control and check for death
    Debug log for displaying death. Bee control using mouse or on Ios touch
    input.

---
+++ Source/Assets/Ground/beeControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beeControl : MonoBehaviour {
+	Vector3 moveToPos;
+
+	Camera myCam;
+
+	void Start()
+	{
+		myCam = transform.parent.camera;
+	}
+
+	void Update()
+	{
+#if UNITY_STANDALONE
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit = new RaycastHit();
+			Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				moveToPos = new Vector3(hit.point.x, transform.position.y, transform.position.z);
+				transform.position = moveToPos;
+			}
+		}
+#endif
+
+#if UNITY_STANDALONE_OSX || UNITY_IPHONE
+		if(Input.touchCount > 0 && Input.GetTouch(0))
+		{
+			RaycastHit hit = new RaycastHit();
+			Ray ray = myCam.ScreenPointToRay(Input.GetTouch(0).position);
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				moveToPos = new Vector3(hit.point.x, transform.position.y, transform.position.z);
+				transform.position = moveToPos;
+			}
+		}
+#endif
+	}
+
+
+
+	void OnTriggerEnter(Collider obj)
+	{
+		if(obj.collider.tag != "path")
+		{
+			// collided with wall
+			Debug.Log("Hit Wall");
+		}
+	}
+}
