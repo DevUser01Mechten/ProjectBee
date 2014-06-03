5d7800a420a99e735860043a7d250f150273ff35
DevUser01Mechten <dev@mechten.eu>
2014-06-03 20:33:33 +0100

    nothing

--- Source/Assets/Ground/beeGUI.cs
+++ Source/Assets/Ground/beeGUI.cs
@@ -5,6 +5,7 @@ public class beeGUI : MonoBehaviour {
 	public float score = 0.0f;
 	public int numLifes = 1;
 	public GUIStyle myStyle;
+	public GUIStyle myButtonStyle;
 
 	int screenW;
 	int screenH;
@@ -38,7 +39,7 @@ public class beeGUI : MonoBehaviour {
 			GUI.Label (new Rect (screenW - (lableW * 1.5f), lableH * 1.5f, lableW, lableH), "Score: " + (int)score, myStyle);
 			GUI.Label (new Rect (lableW * 1.5f, lableH * 1.5f, lableW, lableH), "Lives: " + numLifes, myStyle);
 			//menu
-			if (GUI.Button (new Rect (0f, 0f, lableW, lableH), "MENU", myStyle))
+			if (GUI.Button (new Rect (0f, 0f, lableW, lableH), "MENU", myButtonStyle))
 			{
 				MainMenu.Instance.displayMainMenu = true;
 				Time.timeScale = 0.0000001f;
