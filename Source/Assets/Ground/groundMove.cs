bc651c1391e3a82e0a265160db482c54b91c60f7
DevUser01Mechten <dev@mechten.eu>
2014-06-03 13:17:52 +0100

    Added random maze creation
    Added script to create planes with d right size, and create a random
    maze with 1 path for the bee to follow.
    Bee model also added with flying animation and the camera moves.

---
+++ Source/Assets/Ground/groundMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundMove : MonoBehaviour {
+	float speed = 5;
+	Vector3 tempPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		tempPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z-(speed*Time.deltaTime));
+		transform.position = tempPos;
+	
+	}
+}
---
+++ Source/Assets/Ground/typeInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class typeInfo : MonoBehaviour {
+
+	public int index;
+	public int row;
+	public int col;
+	public float size;
+	public bool isPath;
+}
